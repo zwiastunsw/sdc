2c3c5af02ff97e13ad6c5733b7e385925e21e245
Cezary Tomczyk <cezary.tomczyk@gmail.com>
2025-10-07 21:32:28 +0200

    Update docusaurus.config.ts

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://siec-dostepnosci-cyfrowej.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sdc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'siec-dostepnosci-cyfrowej', // Usually your GitHub org/user name.
+  projectName: 'siec-dostepnosci-cyfrowej.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
