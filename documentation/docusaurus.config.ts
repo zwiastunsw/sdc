a08d2aae26db1eba4731d3920930497212020b1d
Cezary Tomczyk <cezary.tomczyk@gmail.com>
2025-10-07 21:24:11 +0200

    Aktualizacja package.json, docusaurus.config.ts i deploy-docusaurus github workflow

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://siec-dostepnosci-cyfrowej.github.io/sdc/',
+  url: 'https://siec-dostepnosci-cyfrowej.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sdc/',
+  baseUrl: '/documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
