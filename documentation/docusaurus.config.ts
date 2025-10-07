9eb14f681e6e510cdd9b2b95809d60cef0b07828
Cezary Tomczyk <cezary.tomczyk@gmail.com>
2025-10-07 21:18:10 +0200

    Aktualizacja konfiguracji Docusaurus-a; optymalizacja obrazków

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ctomczyk.github.io/',
+  url: 'https://siec-dostepnosci-cyfrowej.github.io/sdc/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus/',
+  baseUrl: '/sdc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
