f9cd420d85701099226dd9dcac89773742d585df
Cezary Tomczyk <cezary.tomczyk@gmail.com>
2025-10-10 12:23:48 +0200

    Aktualizacja translacji

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
     // may want to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'pl',
-        locales: ['pl'],
+        locales: ['pl']
     },
 
     plugins: [
